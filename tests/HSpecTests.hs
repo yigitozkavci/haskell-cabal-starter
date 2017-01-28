7a8032b714f124132647d48776c64af7a264909a
Yiğit Özkavcı <yigitozkavci8@gmail.com>
2017-01-28 21:03:47 +0300

    Add failure spec and set default language for library

--- tests/HSpecTests.hs
+++ tests/HSpecTests.hs
@@ -4,7 +4,10 @@ import Haq
 import Test.Hspec
  
 main :: IO ()
-main = hspec $
+main = hspec $ do
   describe "Validate haqify function" $
     it "haqify is supposed to prefix Haq! to things" $
       haqify "me" `shouldBe` "Haq! me"
+  describe "Falsely validate haqify function" $
+    it "haqify is not supposed to prefix Haq! to things" $
+      haqify "me" `shouldBe` "NonHaq! me"
